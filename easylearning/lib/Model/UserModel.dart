14d7e4c9ad89e4c452628ddab7434dee33a19344
shivanii9718 <shrmashivani9907@gmail.com>
2023-07-13 12:58:55 -0400

    Refactoring code

--- easylearning/lib/Model/UserModel.dart
+++ easylearning/lib/Model/UserModel.dart
@@ -9,15 +9,15 @@ String? password;
 UserModel({this.uid,this.firstName,this.secondName,this.email,this.password});
 
 //getting data from server
-factory UserModel.fromMap(map){
-  return UserModel(
-    uid: map['uid'],
-    firstName: map['firstName'],
-    secondName: map['secondName'],
-    email: map['email'],
-    password: map['password'],
-  );
-}
+// factory UserModel.fromMap(map){
+//   return UserModel(
+//     uid: map['uid'],
+//     firstName: map['firstName'],
+//     secondName: map['secondName'],
+//     email: map['email'],
+//     password: map['password'],
+//   );
+// }
 
 //sending data to server
 Map<String, dynamic> toMap(){

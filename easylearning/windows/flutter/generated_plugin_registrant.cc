c60ab9c8f97feeeba07a5e8e05de180d159e08d5
prabh <prabhnoorsingh23000@gmail.com>
2023-07-13 13:08:33 -0400

    View courses updated 2.0

--- easylearning/windows/flutter/generated_plugin_registrant.cc
+++ easylearning/windows/flutter/generated_plugin_registrant.cc
@@ -12,4 +12,5 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
 }
-// view courses
+// view courses 2.0
+

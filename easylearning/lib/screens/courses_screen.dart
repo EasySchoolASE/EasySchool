cd25801bdd639a3a38c6fb71e5d6827962ac1ce9
prabh <prabhnoorsingh23000@gmail.com>
2023-07-15 11:21:07 -0400

    view courses updated 2.2

--- easylearning/lib/screens/courses_screen.dart
+++ easylearning/lib/screens/courses_screen.dart
@@ -1,10 +1,19 @@
+import 'package:easylearning/screens/login_screens.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../Model/UserModel.dart';
+
 class Course {
   String name;
   String instructor;
   int duration;
   double price;
+  bool isEnrolled;
 
-  Course(this.name, this.instructor, this.duration, this.price);
+  Course(this.name, this.instructor, this.duration, this.price) {
+    isEnrolled = false;
+  }
 }
 
 class Student {
@@ -16,8 +25,23 @@ class Student {
   }
 
   void enrollCourse(Course course) {
-    coursesEnrolled.add(course);
-    print('Enrolled in ${course.name}');
+    if (!course.isEnrolled) {
+      course.isEnrolled = true;
+      coursesEnrolled.add(course);
+      print('Enrolled in ${course.name}');
+    } else {
+      print('Already enrolled in ${course.name}');
+    }
+  }
+
+  void dropCourse(Course course) {
+    if (course.isEnrolled) {
+      course.isEnrolled = false;
+      coursesEnrolled.remove(course);
+      print('Dropped ${course.name}');
+    } else {
+      print('Course ${course.name} not enrolled.');
+    }
   }
 
   void displayEnrolledCourses() {
@@ -45,7 +69,15 @@ void main() {
   student.enrollCourse(course1);
   student.enrollCourse(course2);
   student.enrollCourse(course3);
+  student.enrollCourse(course1); // Trying to enroll in the same course again
 
   // Displaying enrolled courses
+  student.displayEnrolledCourses();
+
+  // Dropping a course
+  student.dropCourse(course2);
+  student.dropCourse(course2); // Trying to drop a course not enrolled
+
+  // Displaying enrolled courses after dropping
   student.displayEnrolledCourses();
 }

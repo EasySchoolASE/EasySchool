9880a489383d0c2a44fc809c82e49f2761ee6dc9
shivanii9718 <shrmashivani9907@gmail>
2023-07-15 21:23:13 -0400

    home screen firebase issue fix

--- easylearning/lib/screens/home_screen.dart
+++ easylearning/lib/screens/home_screen.dart
@@ -17,18 +17,17 @@ class _HomeScreenState extends State<HomeScreen> {
   UserModel loggedInUser = UserModel();
   bool isPressed = false;
 
-
   @override
   void initState() {
     super.initState();
-    // FirebaseFirestore.instance
-    //     .collection("users")
-    //     .doc(user!.uid)
-    //     .get()
-    //     .then((value) {
-    //   loggedInUser = UserModel.fromMap(value.data());
-    //   setState(() {});
-    // });
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
   }
 
   @override
@@ -41,78 +40,81 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text("Welcome"),
           centerTitle: true,
         ),
-        body: 
-        Container(
-        margin: const EdgeInsets.all(10),
-        child:
-        Column(
-          children: [
-            if (isPressed) ...{
-              const SizedBox(
-                height: 10,
+        body: Container(
+            margin: const EdgeInsets.all(10),
+            child: Column(children: [
+              if (isPressed) ...{
+                const SizedBox(
+                  height: 10,
+                ),
+                Table(
+                    border: TableBorder.all(
+                        color: const Color.fromRGBO(59, 107, 170, 1),
+                        style: BorderStyle.solid,
+                        width: 2),
+                    children: [
+                      TableRow(
+                          decoration: const BoxDecoration(
+                            color: Color.fromRGBO(59, 107, 170, 1),
+                          ),
+                          children: [
+                            Column(children: const [
+                              Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text('ID',
+                                      style: TextStyle(fontSize: 20.0)))
+                            ]),
+                            Column(children: const [
+                              Text('Key', style: TextStyle(fontSize: 20.0))
+                            ]),
+                            Column(children: const [
+                              Text('Date', style: TextStyle(fontSize: 20.0))
+                            ]),
+                            Column(children: const [
+                              Text('Sold', style: TextStyle(fontSize: 20.0))
+                            ]),
+                          ]),
+                    ]),
+              },
+              Expanded(child: Container()),
+              Align(
+                alignment: FractionalOffset.center,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              textStyle: const TextStyle(fontSize: 25)),
+                          onPressed: () async {
+                            isPressed = true;
+                            setState(() {});
+                          },
+                          child: const Text("Welcome!")),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              textStyle: const TextStyle(fontSize: 25)),
+                          onPressed: () {
+                            logout(context);
+                            setState(() {});
+                          },
+                          child: const Text("Logout"))
+                    ]),
               ),
-              Table(
-                  border: TableBorder.all(
-                      color: const Color.fromRGBO(59, 107, 170, 1),
-                      style: BorderStyle.solid,
-                      width: 2),
-                  children: [
-                    TableRow(
-                        decoration: const BoxDecoration(
-                          color: Color.fromRGBO(59, 107, 170, 1),
-                        ),
-                        children: [
-                          Column(children: const [
-                            Padding(
-                                padding: EdgeInsets.all(5),
-                                child: Text('ID',
-                                    style: TextStyle(fontSize: 20.0)))
-                          ]),
-                          Column(children: const [
-                            Text('Key', style: TextStyle(fontSize: 20.0))
-                          ]),
-                          Column(children: const [
-                            Text('Date', style: TextStyle(fontSize: 20.0))
-                          ]),
-                          Column(children: const [
-                            Text('Sold', style: TextStyle(fontSize: 20.0))
-                          ]),
-                        ]),
-                  ]),
-            },
-             Expanded(child:Container()),
-            Align(
-              alignment: FractionalOffset.center,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(elevation: 5,textStyle: const TextStyle(fontSize: 25)),
-                        onPressed: () async {
-                          isPressed = true;
-                          setState(() {});
-                        },
-                        child: const Text("Welcome!")),
-                    ElevatedButton(
-                       style: ElevatedButton.styleFrom(elevation: 5,textStyle: const TextStyle(fontSize: 25)),
-                        onPressed: (){
-                          logout(context);
-                          setState(() {});
-                        },
-                        child: const Text("Logout"))
-                  ]),
-            ),
-            const SizedBox(height: 100,)])));
+              const SizedBox(
+                height: 100,
+              )
+            ])));
   }
 }
 
-
 Future<void> logout(BuildContext context) async {
-    await FirebaseAuth.instance.signOut();
-    // ignore: use_build_context_synchronously
-    final prefs=await SharedPreferences.getInstance();
-    prefs.setBool('isLoggedIn', false);
-    // ignore: use_build_context_synchronously
-    await Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const LoginScreen()));
-  }
+  await FirebaseAuth.instance.signOut();
+  // ignore: use_build_context_synchronously
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setBool('isLoggedIn', false);
+  // ignore: use_build_context_synchronously
+  await Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const LoginScreen()));
+}

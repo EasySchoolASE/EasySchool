e0ce24259e78691b3ca5b708661dd9ab34a81e9c
navjot5263 <navikaurtaak@gmail.com>
2023-07-22 23:37:22 -0400

    cleaned up code, removed duplicate build methods, fixed super.key

--- easylearning/lib/main.dart
+++ easylearning/lib/main.dart
@@ -2,21 +2,22 @@ import 'package:easylearning/screens/home_screen.dart';
 import 'package:easylearning/screens/login_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final prefs=await SharedPreferences.getInstance();
-  final isLoggedIn=prefs.getBool('isLoggedIn')??false;
+  final prefs = await SharedPreferences.getInstance();
+  final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
   await Firebase.initializeApp();
-  runApp(MyApp(isLoggedIn:isLoggedIn));
+  runApp(MyApp(isLoggedIn: isLoggedIn));
 }
 
 class MyApp extends StatelessWidget {
   final bool isLoggedIn;
-  const MyApp({super.key, required this.isLoggedIn});
 
-  // This widget is the root of your application.
+  const MyApp({required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,28 +25,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Quicksand',
         colorScheme: ColorScheme.fromSwatch().copyWith(
-      primary:  const Color.fromRGBO(59,107, 170, 1),
-    ),
+          primary: const Color.fromRGBO(59, 107, 170, 1),
+        ),
       ),
+      builder: EasyLoading.init(),
       debugShowCheckedModeBanner: false,
-      home: isLoggedIn? const HomeScreen():  const LoginScreen(),
-    );
-  }
-}
-
-
- @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Easy School',
-      theme: ThemeData(
-        fontFamily: 'Quicksand',
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-      primary:  const Color.fromRGBO(59,107, 170, 1),
-    ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: isLoggedIn? const HomeScreen():  const LoginScreen(),
+      home: isLoggedIn ? const HomeScreen() : const LoginScreen(),
     );
   }
 }
